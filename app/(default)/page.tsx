c761e6e76868ea2c70ebc51c63eb383a55044e15
cristianmeelo <cristianmeelo@gmail.com>
2023-09-07 16:38:46 -0300

    :sparkles: feat: remove useless components

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,11 +1,3 @@
-import type { Metadata } from 'next'
- 
-export const metadata: Metadata = {
-  title: "Consultoria em TCC",
-  description: "Página para orçar consultoria de TCC",
-}
-
-
 import Hero from "@/components/hero";
 import Features from "@/components/features";
 import Testimonials from "@/components/testimonials";
