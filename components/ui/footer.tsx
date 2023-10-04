d0bc4b95f683a3e121678eeb9753848338fe6f6d
cristianmeelo <cristianmeelo@gmail.com>
2023-10-03 22:52:30 -0300

    Update footer.tsx

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -56,9 +56,9 @@ export default function Footer() {
           </div>
         </div>
       </div>
-      <a href="https://www.facebook.com/sharer.php?u=https://babirevisao.vercel.app/" target="_blank" rel="noopener noreferrer">
+      {/* <a href="https://www.facebook.com/sharer.php?u=https://babirevisao.vercel.app/" target="_blank" rel="noopener noreferrer">
         Compartilhar no Facebook
-      </a>
+      </a> */}
     </footer>
   );
 }
