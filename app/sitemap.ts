7249d75c914197513f260da933fac1cfa7b488a6
cristianmeelo <cristianmeelo@gmail.com>
2023-10-03 22:27:34 -0300

    :sparkles: feat: update sitemap to xml format

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,9 +1,30 @@
-export default function sitemap() {
+const generateSitemapXml = () => {
   const baseUrl = "https://babirevisao.vercel.app";
-  return [
-    {
-      url: baseUrl,
-      lastModified: new Date(),
-    },
-  ];
-}
+  const pages = [""]; // Adicione outras páginas se necessário
+
+  const sitemap = `
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${pages
+        .map(
+          (page) => `
+        <url>
+          <loc>${baseUrl}${page}</loc>
+          <lastmod>${new Date().toISOString()}</lastmod>
+        </url>`
+        )
+        .join("")}
+    </urlset>
+  `;
+
+  return sitemap;
+};
+
+const Sitemap = () => {};
+
+Sitemap.getInitialProps = ({ res }: any) => {
+  res.setHeader("Content-Type", "text/xml");
+  res.write(generateSitemapXml());
+  res.end();
+};
+
+export default Sitemap;
