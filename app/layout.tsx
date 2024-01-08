825d98543f28c5c1b4d1329ef8d7f70ded2b1176
cristianmeelo <cristianmeelo@gmail.com>
2024-01-07 22:08:55 -0300

    Revert ":sparkles: feat: try to implement hotjar in another way"
    This reverts commit 57c1b0fd944ad29fa2dc04d25ce647928152e036.

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,18 +78,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   gtag('config', 'G-91XQY9G9D5');`,
           }}
         />
-
-{/* <!-- Hotjar Tracking Code for https://babirevisao.vercel.app --> */}
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:3816544,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
       </head>
       <link rel="icon" href="https://babirevisao.vercel.app/favicon.ico" type="image/x-icon" />
       <link rel="shortcut icon" href="https://babirevisao.vercel.app/favicon.ico" type="image/x-icon" />
